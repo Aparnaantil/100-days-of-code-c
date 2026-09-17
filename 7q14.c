1ebbb3852433e79fd3b4c170953215a6366078ea
Aparnaantil <Aparna.40278@stu.upes.ac.in>
2026-09-18 02:08:02 +0530

    Add files via upload

---
+++ 7q14.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main() 
+{
+    char ch;
+    if (scanf(" %c", &ch) != 1) 
+{
+        return 1;
+    }
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        printf("Vowel\n");
+    } else {
+        printf("Consonant\n");
+    }
+return 0;
+}
