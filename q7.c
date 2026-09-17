f84c47693ad97b7e014506ec94bb65fa19b0b540
Aparnaantil <Aparna.40278@stu.upes.ac.in>
2026-09-18 00:51:33 +0530

    Add files via upload

---
+++ q7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+    int num1, num2;
+    if (scanf("%d %d", &num1, &num2) == 2)
+{
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+       num1 = num1 - num2;
+        printf("After swap: %d %d\n", num1, num2);
+    }
+    return 0;
+}
