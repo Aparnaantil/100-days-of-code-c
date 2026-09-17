391c4d36512884a51ebb6653c3de2429821d3651
Aparnaantil <Aparna.40278@stu.upes.ac.in>
2026-09-18 01:34:18 +0530

    Add files via upload

---
+++ q10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+    int total_seconds;
+    int hours, minutes, seconds;
+    if (scanf("%d", &total_seconds) != 1) {
+        return 1;
+    }
+    hours = total_seconds / 3600;
+    minutes = (total_seconds % 3600) / 60;
+    seconds = total_seconds % 60;
+    printf("%d:%d:%d\n", hours, minutes, seconds);
+    return 0;
+}
