7c3034561b53a9d24c6187ec618e2eeb151623b5
Aparnaantil <Aparna.40278@stu.upes.ac.in>
2026-09-18 01:38:37 +0530

    Add files via upload

---
+++ q11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    int num;
+    if (scanf("%d", &num) != 1)
+{
+    return 1;
+    }
+    if (num % 2 == 0) {
+        printf("%d is even\n", num);
+    } else {
+        printf("%d is odd\n", num);
+    }
+    return 0;
+}
