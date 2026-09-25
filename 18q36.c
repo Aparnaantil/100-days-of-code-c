5bf5727db75522208777bc3ba8fde35b12bdcbee
Aparnaantil <Aparna.40278@stu.upes.ac.in>
2026-09-25 08:16:33 +0530

    Add files via upload

---
+++ 18q36.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() 
+{
+int num1, num2, hcf = 1;
+scanf("%d %d", &num1, &num2);
+int min = num1;
+if (num2 < num1) 
+{
+min = num2;
+}
+for (int i = 1; i <= min; i++) 
+{
+if (num1 % i == 0 && num2 % i == 0) 
+{
+hcf = i; 
+}
+}
+printf("%d\n", hcf);
+return 0;
+}
+
